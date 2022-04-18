2cdb7c1c47c978a59442189ffcc44ca9425ad4d3
DriftkidErick <94418693+DriftkidErick@users.noreply.github.com>
2022-04-18 18:56:58 -0400

    Add files via upload

---
+++ Cordon_1A.py
@@ -0,0 +1,39 @@
+#W2D2 lAB 1A
+
+#Erick Cordon
+#Lab 1A
+
+#program PROMT/ PURPOSE: Write a solution file that displays the following output. Use variables to hold the month, day, and year values. (You may change the date to the due date of this lab) 
+
+
+#MAIN CODE BELOW ------------------------------------------------------------------------
+
+print("Hello World! This is my first programming program :]") #intro to my first line of code
+
+print() #left blank to create a space from intro to H
+
+print( "     HH      HH   II    !!!")
+
+print( "     HH      HH         !!!")
+
+print( "     HH      HH   II    !!!")
+
+print( "     HHHHHHHHHH   II    !!!")   #H's added in the middle to fill the space
+
+print( "     HH      HH   II    !!!")
+
+print( "     HH      HH   II    !!!")
+
+print( "     HH      HH   II     !")
+
+print() #left blank to create a space from H to date
+
+m=4 # represents the month of the due date
+
+d=10 # represents the day of the due date
+
+y=2022 # represents the year of the due date
+
+print(f"          Date: {m} - {d} - {y}")
+# Represents the due date of the assignment   
+
